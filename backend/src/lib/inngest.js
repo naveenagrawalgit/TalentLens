7616b70766ce195804f828943b796a5d58677796
naveenagrawalgit <naveenagrawal1008@gmail.com>
2026-01-27 22:43:17 +0530

    imported user

--- backend/src/lib/inngest.js
+++ backend/src/lib/inngest.js
@@ -1,6 +1,6 @@
 import { Inngest } from "inngest";
 import { connectDB } from "./DB.js";
-
+import {User} from "../models/User.model.js"
 // Create a client to send and receive events
 export const inngest = new Inngest({ id: "TalentLens" });
 
